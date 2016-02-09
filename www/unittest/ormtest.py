15d999c1ba417dacecdb83b282ca502b4c64b137
bluescv <bluescv@gmail.com>
2016-02-09 17:05:51 +0800

    add some orm code

---
+++ www/unittest/ormtest.py
@@ -0,0 +1,18 @@
+import www.orm
+from www.models import User
+import asyncio
+
+loop = asyncio.get_event_loop()
+
+
+async def test():
+    # yield from www.orm.create_pool(loop, user='www-data', password='www-data', db='awesome')
+    await www.orm.create_pool(loop, user='www-data', password='www-data', db='awesome')
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    await u.save()
+
+
+print('ormtest start')
+loop.run_until_complete(test())
